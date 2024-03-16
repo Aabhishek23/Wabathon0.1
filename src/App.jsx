a503102a1a994508f9d65504f780dbe928641824
TaniyaParochi <taniyaparochi6@gmail.com>
2024-03-16 20:42:10 +0530

    square add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './App.css'
 import './index.css'
+import Square from "./Square/Square"
 
 
 const App = () => {
@@ -10,6 +11,11 @@ const App = () => {
     <div className="main-div">
       <div>
         <h1 className="game-heading water-background">My First React App</h1>
+      
+      <div className="square-wrapper">
+      <Square />  
+      </div>
+      
       </div>
 
     </div>
